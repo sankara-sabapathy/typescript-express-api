aea2b6ac39287d3af14562abb8e6dcd5b9326762
sankara sabapathy <krssabapathy1999@gmail.com>
2023-09-04 21:34:12 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ const app = express();
 const api = Router();
 
 api.get("/hello", (req, res) => {
-  return res.status(200).send({ message: "Hello from the public api!" });
+  return res.status(200).send({ message: "Hello from the public apii!" });
 });
 
 api.get("/greet/:name", (req, res) => {
